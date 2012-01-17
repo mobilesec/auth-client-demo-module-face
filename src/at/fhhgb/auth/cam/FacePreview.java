71707981ec881b2e78822f6c3c877c9882113019
Thomas Kaiser <thomas.kaiser.bsc@gmail.com>
2012-01-17 11:41:06 +0000

    temporary project for experimenting with camera preview

---
+++ src/at/fhhgb/auth/cam/FacePreview.java
@@ -0,0 +1,296 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package at.fhhgb.auth.cam;
+
+import static com.googlecode.javacv.cpp.opencv_core.IPL_DEPTH_8U;
+import static com.googlecode.javacv.cpp.opencv_core.cvClearMemStorage;
+import static com.googlecode.javacv.cpp.opencv_core.cvGetSeqElem;
+import static com.googlecode.javacv.cpp.opencv_core.cvLoad;
+import static com.googlecode.javacv.cpp.opencv_objdetect.CV_HAAR_DO_CANNY_PRUNING;
+import static com.googlecode.javacv.cpp.opencv_objdetect.cvHaarDetectObjects;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ImageFormat;
+import android.graphics.Paint;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.Size;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+import com.googlecode.javacpp.Loader;
+import com.googlecode.javacv.cpp.opencv_core.CvMemStorage;
+import com.googlecode.javacv.cpp.opencv_core.CvRect;
+import com.googlecode.javacv.cpp.opencv_core.CvSeq;
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
+import com.googlecode.javacv.cpp.opencv_objdetect;
+import com.googlecode.javacv.cpp.opencv_objdetect.CvHaarClassifierCascade;
+
+// ----------------------------------------------------------------------
+
+public class FacePreview extends Activity {
+    private FrameLayout layout;
+    private FaceView faceView;
+    private Preview mPreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        
+        // Hide the window title.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // Create our Preview view and set it as the content of our activity.
+        try {
+            layout = new FrameLayout(this);
+            faceView = new FaceView(this);
+            mPreview = new Preview(this, faceView);
+            layout.addView(mPreview);
+            layout.addView(faceView);
+            setContentView(layout);
+        } catch (IOException e) {
+            e.printStackTrace();
+            new AlertDialog.Builder(this).setMessage(e.getMessage()).create().show();
+        }
+    }
+}
+
+// ----------------------------------------------------------------------
+
+class FaceView extends View implements Camera.PreviewCallback {
+    public static final int SUBSAMPLING_FACTOR = 4;
+
+    private IplImage grayImage;
+    private CvHaarClassifierCascade classifier;
+    private CvMemStorage storage;
+    private CvSeq faces;
+
+    public FaceView(FacePreview context) throws IOException {
+        super(context);
+        Loader.load(opencv_objdetect.class); 
+        File classifierFile = loadClassifier(context);
+        if (classifierFile == null || classifierFile.length() <= 0) {
+            throw new IOException("Could not extract the classifier file from Java resource.");
+        }
+
+        // Preload the opencv_objdetect module to work around a known bug.
+        Loader.load(opencv_objdetect.class);
+        classifier = new CvHaarClassifierCascade(cvLoad(classifierFile.getAbsolutePath()));
+        classifierFile.delete();
+        if (classifier.isNull()) {
+            throw new IOException("Could not load the classifier file.");
+        }
+        storage = CvMemStorage.create();
+    }
+
+	private File loadClassifier(FacePreview context) throws IOException {
+		InputStream is = context.getResources().openRawResource(R.raw.haarcascade_frontalface_default);
+        File cascadeDir = context.getDir("cascade", Context.MODE_PRIVATE);
+        File cascadeFile = new File(cascadeDir, "haarcascade_frontalface_default.xml");
+        FileOutputStream os = new FileOutputStream(cascadeFile);
+
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+        while ((bytesRead = is.read(buffer)) != -1) {
+            os.write(buffer, 0, bytesRead);
+        }
+        is.close();
+        os.close();
+        return cascadeFile;
+	}
+
+    public void onPreviewFrame(final byte[] data, final Camera camera) {
+        try {
+            Camera.Size size = camera.getParameters().getPreviewSize();
+            processImage(data, size.width, size.height);
+            camera.addCallbackBuffer(data);
+        } catch (RuntimeException e) {
+            // The camera has probably just been released, ignore.
+        }
+    }
+
+    protected void processImage(byte[] data, int width, int height) {
+        // First, downsample our image and convert it into a grayscale IplImage
+        int f = SUBSAMPLING_FACTOR;
+        if (grayImage == null || grayImage.width() != width/f || grayImage.height() != height/f) {
+            grayImage = IplImage.create(width/f, height/f, IPL_DEPTH_8U, 1);
+        }
+        int imageWidth  = grayImage.width();
+        int imageHeight = grayImage.height();
+        int dataStride = f*width;
+        int imageStride = grayImage.widthStep();
+        ByteBuffer imageBuffer = grayImage.getByteBuffer();
+        for (int y = 0; y < imageHeight; y++) {
+            int dataLine = y*dataStride;
+            int imageLine = y*imageStride;
+            for (int x = 0; x < imageWidth; x++) {
+                imageBuffer.put(imageLine + x, data[dataLine + f*x]);
+            }
+        }
+
+        faces = cvHaarDetectObjects(grayImage, classifier, storage, 1.1, 3, CV_HAAR_DO_CANNY_PRUNING);
+        postInvalidate();
+        cvClearMemStorage(storage);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setTextSize(20);
+
+        String s = "FacePreview - This side up.";
+        float textWidth = paint.measureText(s);
+        canvas.drawText(s, (getWidth()-textWidth)/2, 20, paint);
+
+        if (faces != null) {
+            paint.setStrokeWidth(2);
+            paint.setStyle(Paint.Style.STROKE);
+            float scaleX = (float)getWidth()/grayImage.width();
+            float scaleY = (float)getHeight()/grayImage.height();
+            int total = faces.total();
+            for (int i = 0; i < total; i++) {
+                CvRect r = new CvRect(cvGetSeqElem(faces, i));
+                int x = r.x(), y = r.y(), w = r.width(), h = r.height();
+                canvas.drawRect(x*scaleX, y*scaleY, (x+w)*scaleX, (y+h)*scaleY, paint);
+            }
+        }
+    }
+}
+
+// ----------------------------------------------------------------------
+
+class Preview extends SurfaceView implements SurfaceHolder.Callback {
+    SurfaceHolder mHolder;
+    Camera mCamera;
+    Camera.PreviewCallback previewCallback;
+
+    Preview(Context context, Camera.PreviewCallback previewCallback) {
+        super(context);
+        this.previewCallback = previewCallback;
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        mCamera = Camera.open(findFrontCameraId());
+        try {
+           mCamera.setPreviewDisplay(holder);
+        } catch (IOException exception) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+    
+	private int findFrontCameraId() {
+		int numberOfCameras = Camera.getNumberOfCameras();
+
+		CameraInfo cameraInfo = new CameraInfo();
+		for (int i = 0; i < numberOfCameras; i++) {
+			Camera.getCameraInfo(i, cameraInfo);
+			if (cameraInfo.facing == CameraInfo.CAMERA_FACING_FRONT) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Surface will be destroyed when we return, so stop the preview.
+        // Because the CameraDevice object is not a shared resource, it's very
+        // important to release it when the activity is paused.
+        mCamera.stopPreview();
+        mCamera.release();
+        mCamera = null;
+    }
+
+
+    private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.05;
+        double targetRatio = (double) w / h;
+        if (sizes == null) return null;
+
+        Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        // Try to find an size match aspect ratio and size
+        for (Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        // Cannot find the one match the aspect ratio, ignore the requirement
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // Now that the size is known, set up the camera parameters and begin
+        // the preview.
+        Camera.Parameters parameters = mCamera.getParameters();
+
+        List<Size> sizes = parameters.getSupportedPreviewSizes();
+        Size optimalSize = getOptimalPreviewSize(sizes, w, h);
+        parameters.setPreviewSize(optimalSize.width, optimalSize.height);
+
+        mCamera.setParameters(parameters);
+        if (previewCallback != null) {
+            mCamera.setPreviewCallbackWithBuffer(previewCallback);
+            Camera.Size size = parameters.getPreviewSize();
+            byte[] data = new byte[size.width*size.height*
+                    ImageFormat.getBitsPerPixel(parameters.getPreviewFormat())/8];
+            mCamera.addCallbackBuffer(data);
+        }
+        mCamera.startPreview();
+    }
+
+}
---
+++ src/at/fhhgb/auth/cam/PreviewDemo.java
@@ -0,0 +1,172 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package at.fhhgb.auth.cam;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.Face;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Toast;
+
+public class PreviewDemo extends Activity {
+	protected static final String TAG = "PreviewDemo";
+	private SurfaceView preview = null;
+	private SurfaceHolder previewHolder = null;
+	private Camera camera = null;
+	private boolean inPreview = false;
+	private int frontCameraId;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		preview = (SurfaceView) findViewById(R.id.preview);
+		previewHolder = preview.getHolder();
+		previewHolder.addCallback(surfaceCallback);
+		previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		
+		frontCameraId = findFrontCameraId();
+	}
+	
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		camera = Camera.open(frontCameraId);
+		camera.setDisplayOrientation(90);
+		camera.setFaceDetectionListener(new MyFaceDetectionListener());
+	}
+
+	private int findFrontCameraId() {
+		int numberOfCameras = Camera.getNumberOfCameras();
+
+		CameraInfo cameraInfo = new CameraInfo();
+		for (int i = 0; i < numberOfCameras; i++) {
+			Camera.getCameraInfo(i, cameraInfo);
+			if (cameraInfo.facing == CameraInfo.CAMERA_FACING_FRONT) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
+
+	@Override
+	public void onPause() {
+		if (inPreview) {
+			camera.stopPreview();
+		}
+
+		camera.release();
+		camera = null;
+		inPreview = false;
+
+		super.onPause();
+	}
+
+	private Camera.Size getBestPreviewSize(int width, int height,
+			Camera.Parameters parameters) {
+		Camera.Size result = null;
+
+		for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+			if (size.width <= width && size.height <= height) {
+				if (result == null) {
+					result = size;
+				} else {
+					int resultArea = result.width * result.height;
+					int newArea = size.width * size.height;
+
+					if (newArea > resultArea) {
+						result = size;
+					}
+				}
+			}
+		}
+
+		return (result);
+	}
+	
+	public void startFaceDetection(){
+	    Camera.Parameters params = camera.getParameters();
+
+	    // start face detection only *after* preview has started
+	    if (params.getMaxNumDetectedFaces() > 0){
+	        camera.startFaceDetection();
+	    }
+	}
+	
+	class MyFaceDetectionListener implements Camera.FaceDetectionListener {
+
+	    @Override
+	    public void onFaceDetection(Face[] faces, Camera camera) {
+	        if (faces.length > 0){
+	            Log.d(TAG, "face detected: "+ faces.length +
+	                    " Face 1 Location X: " + faces[0].rect.centerX() +
+	                    "Y: " + faces[0].rect.centerY() );
+	        }
+	    }
+	}
+
+	SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+		public void surfaceCreated(SurfaceHolder holder) {
+			try {
+				camera.setPreviewDisplay(previewHolder);
+				camera.startPreview();
+				startFaceDetection();
+			} catch (Throwable t) {
+				Log.e("PreviewDemo-surfaceCallback",
+						"Exception in setPreviewDisplay()", t);
+				Toast.makeText(PreviewDemo.this, t.getMessage(),
+						Toast.LENGTH_LONG).show();
+			}
+		}
+
+		public void surfaceChanged(SurfaceHolder holder, int format, int width,
+				int height) {
+			Camera.Parameters parameters = camera.getParameters();
+			Camera.Size size = getBestPreviewSize(width, height, parameters);
+			Log.d(TAG, "Found best preview size: " + size.width + "/" + size.height + " for width/height:" 
+					+ width + "/" + height);
+
+			if (size != null) {
+				parameters.setPreviewSize(size.width, size.height);
+				camera.setParameters(parameters);
+				camera.startPreview();
+
+				startFaceDetection();
+				inPreview = true;
+			}
+		}
+
+		public void surfaceDestroyed(SurfaceHolder holder) {
+			// no-op
+		}
+	};
+	
+	public class DummyWrapperView extends View {
+
+		public DummyWrapperView(Context context) {
+			super(context);
+		}
+		
+	}
+}
